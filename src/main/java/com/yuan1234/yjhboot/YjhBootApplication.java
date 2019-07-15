b46be0e0bbba761b6602976412b840658f55c402
yaominyuan <2420086222@qq.com>
2019-07-15 16:27:56 +0800

    初始化项目

---
+++ src/main/java/com/yuan1234/yjhboot/YjhBootApplication.java
@@ -0,0 +1,13 @@
+package com.yuan1234.yjhboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjhBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjhBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuan1234/yjhboot/YjhBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuan1234.yjhboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YjhBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
